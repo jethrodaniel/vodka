84e0ac1147ba7ce639b351d41a5f7de08379d4c1
Mark Delk <jethrodaniel@gmail.com>
2019-06-12 07:21:24 -0500

    tmp

--- src/lib.rs
+++ src/lib.rs
@@ -1,10 +1,26 @@
+use std::env;
+use std::process;
+
 use std::ptr;
 use x11::xlib::{XCloseDisplay, XOpenDisplay, XScreenCount, XScreenOfDisplay};
 
 pub fn resolution() {
     unsafe {
+        let display_env = match env::var("DISPLAY") {
+            Ok(value) => {
+              println!("DISPLAY: {:?}", value);
+              value
+            },
+            Err(_) => {
+                println!("$DISPLAY must be set! Aborting...");
+                process::exit(1);
+            },
+        };
+        println!("DISPLAY: {}", display_env);
+
         // open a display
         let display = XOpenDisplay(ptr::null());
+
         // return the number of available screens
         let count_screens = XScreenCount(display);
 
@@ -13,6 +29,7 @@ pub fn resolution() {
             let screen = *XScreenOfDisplay(display, i);
             println!("\tScreen {}: {}x{}", i + 1, screen.width, screen.height);
         }
+
         // close the display
         XCloseDisplay(display);
     }
